f068597b8b1a454b8cda69f6ee5afade98dd9ae5
Surawut Saithong-in <surawut.kong@gmail.com>
2022-08-05 22:21:17 +0700

    5. Components & States

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import WordCard from './WordCard';
-//const word = "Hello";
+const word = "HELLO";
 
 function App() {
     return (
         <div>
-        <WordCard value="hello"/>
+        <WordCard value={ word }/>
         </div>
     );
 }
